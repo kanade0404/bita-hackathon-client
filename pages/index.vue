92027ad3347b2d52c4e9c83f24c6b2c95ef93568
MakotoTatsumi <sakuchan.jp@gmail.com>
2021-04-10 15:42:35 +0900

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -27,10 +27,29 @@
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+export default {
+  methods: {
+    getLocation() {
+      axios.defaults.headers.get['Content-Type'] =
+        'application/json;charset=utf-8'
+      axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*'
+      axios({
+        method: 'get',
+        url:
+          'https://maps.googleapis.com/maps/api/place/findplacefromtext/json',
+        data: {
+          input: 'Museum%20of%20Contemporary%20Art%20Australia',
+          key: '',
+          inputtype: 'textquery',
+        },
+      })
+    },
+  },
+}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .container {
   margin: 0 auto;
   min-height: 100vh;
